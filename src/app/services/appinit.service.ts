6aa3382eef8c8188e72b2f430c5e2fd82df43a24
John Pankowicz <johnpankowicz@hotmail.com>
2021-03-09 17:34:30 -0500

    refactor

--- src/app/services/appinit.service.ts
+++ src/app/services/appinit.service.ts
@@ -2,54 +2,58 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
 
-let server = "http://sosuchhostanywhere.org";
+let server = "http://no-such-web-server.org";
 //
 // let server = "https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=perl&site=stackoverflow";
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class AppInitService {
+
   constructor(private httpClient: HttpClient) {}
+
   static isRunning: boolean | null = null;
 
   static async isWebServerRunning() {
-    console.log("AppInitService:isWebServerRunning Enter ", this.getNow());
-    console.log("AppInitService:isWebServerRunning type of isRunning = ", typeof (this.isRunning) )
+    logMsg("isWebServerRunning Enter");
     const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-    // check every 50 milliseconds
+    // check every 50 milliseconds for change in "isRunning" status
     while (AppInitService.isRunning === null) {
-      console.log("AppInitService:isWebServerRunning in delay loop", this.getNow());
+      logMsg("isWebServerRunning in delay loop");
       await delay(50);
     }
-    let running = AppInitService.isRunning;
-    console.log('AppInitService:isWebServerRunning Exit -isRunnning = ' + String(running), this.getNow());
-    return running;
+    let isrunning = AppInitService.isRunning;
+    logMsg("isWebServerRunning Exit -isRunnning = " + String(isrunning));
+    return isrunning;
   }
 
   pingServer(): Promise<any> {
-    console.log('AppInitService:pingServer. Enter ', AppInitService.getNow());
+    logMsg("pingServer. Enter");
 
     const promise = this.httpClient
       .get(server)
       .toPromise()
       .then((settings) => {
-        console.log('AppInitService:pingServer Got server response', AppInitService.getNow());
+        logMsg("pingServer Got server response");
         AppInitService.isRunning = true;
         return true;
       })
       .catch((err) => {
-        console.log('AppInitService:pingServer No server Response', AppInitService.getNow());
+        logMsg("pingServer No server Response");
         AppInitService.isRunning = false;
         err;
       });
-
     return promise;
   }
-
-  static getNow() {
-    let now = Date.now();
-    let sec = (Math.floor(now / 1000)) % 100;
-    let ms = now % 1000;
-    return sec.toString() + ":" + ms.toString();
-  }
 }
+
+function logMsg(msg: string) {
+  console.log('AppInitService:', msg, getNow());}
+
+function getNow() {
+  let now = Date.now();
+  let sec = (Math.floor(now / 1000)) % 100;
+  let ms = now % 1000;
+  return sec.toString() + ":" + ms.toString();
+}
+
